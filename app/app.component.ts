8e76f5bfdd80bc59f5f23a9f06153276cca8238f
Habib <Michael.Habib@marcusmillichap.com>
2017-04-19 12:42:03 -0700

    Bootstrap application

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by mhabib on 4/19/2017.
+ */
+
+import { Component } from "@angular/core";
+
+@Component({
+    selector:"app",
+    template:`<div class="bg-success p-a-1 text-xs-center">
+        This is SportsStore
+    </div>`
+})
+
+export class AppComponent{}
---
+++ app/main.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by mhabib on 4/19/2017.
+ */
+
+import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
+import { AppModule } from "./app.module"
+
+platformBrowserDynamic().bootstrapModule(AppModule);
