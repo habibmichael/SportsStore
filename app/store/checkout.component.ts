2a0de5e04374700c4fa358396c8e8ace941b4869
Michael Habib <michaelhabib1868@gmail.com>
2017-04-22 13:10:00 -0700

    Add routing to project

---
+++ app/store/checkout.component.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by mh122354 on 4/22/2017.
+ */
+
+import { Component } from "@angular/core";
+
+@Component({
+    template: `<div><h3 class="bg-info p-a-1">Checkout Component</h3></div>`
+})
+
+export class CheckoutComponent{}
