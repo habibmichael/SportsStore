d6ea10fe988a0d3e4af207d4c211be9dc020a184
Michael Habib <michaelhabib1868@gmail.com>
2017-04-22 16:45:59 -0700

    use rest data source in place of dummy data

--- app/model/model.module.ts
+++ app/model/model.module.ts
@@ -8,10 +8,14 @@ import { StaticDataSource } from "./static.datasource";
 import { Cart } from "./cart.model";
 import { Order } from "./order.model";
 import { OrderRepository } from "./order.repository";
+import { RestDataSource } from "./rest.datasource";
+import { HttpModule } from "@angular/http";
 
 @NgModule({
+    imports: [HttpModule],
     providers:[ProductRepository,StaticDataSource, Cart,
-               Order, OrderRepository]
+               Order, OrderRepository,
+        {provide: StaticDataSource, useClass: RestDataSource}]
 })
 
 export class ModelModule{ }
