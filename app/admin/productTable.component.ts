3942eba5e5268ea12f4ea0f7f9f45c230c83584c
Michael Habib <michaelhabib1868@gmail.com>
2017-05-11 23:41:59 -0700

    implement product feature

--- app/admin/productTable.component.ts
+++ app/admin/productTable.component.ts
@@ -2,9 +2,23 @@
  * Created by mh122354 on 4/27/2017.
  */
 import { Component } from "@angular/core";
+import { Product } from '../model/product.model';
+import { ProductRepository } from '../model/product.repository';
 
 @Component({
-    template: `<div class="bg-info p-a-1"><h3>Product Table Placeholder</h3></div>`
+    moduleId: module.id,
+    templateUrl: 'productTable.component.html'
 })
 
-export class ProductTableComponent{}
+export class ProductTableComponent{
+
+    constructor(private repository: ProductRepository){}
+
+    getProducts(): Product[] {
+        return this.repository.getProducts();
+    }
+
+    deleteProduct(id: number){
+        this.respository.deleteProduct(id);
+    }
+}
